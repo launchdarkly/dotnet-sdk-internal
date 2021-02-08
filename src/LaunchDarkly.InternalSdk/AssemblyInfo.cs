a782cf44095117a805eef98ca25fba18513e22da
Eli Bishop <eli@launchdarkly.com>
2021-02-08 10:22:51 -0800

    fix release build (disable InternalsVisibleTo that's used only in tests)

--- src/LaunchDarkly.InternalSdk/AssemblyInfo.cs
+++ src/LaunchDarkly.InternalSdk/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Runtime.CompilerServices;
 
 #if DEBUG
 [assembly: InternalsVisibleTo("LaunchDarkly.InternalSdk.Tests")]
-#endif
 
 // Allow mock/proxy objects in unit tests to access internal classes
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+#endif
