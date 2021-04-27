06a94696a3b357a210fa60326c66804736f0429a
Eli Bishop <eli@launchdarkly.com>
2021-04-27 12:15:12 -0700

    add UriExtensions, remove WireMock from tests, misc cleanup (#17)

--- test/LaunchDarkly.InternalSdk.Tests/TestUtil.cs
+++ test/LaunchDarkly.InternalSdk.Tests/TestUtil.cs
@@ -1,11 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using LaunchDarkly.Logging;
-using WireMock.Logging;
-using WireMock.Server;
-using WireMock.Settings;
 using Xunit;
 
 namespace LaunchDarkly.Sdk
@@ -25,41 +20,6 @@ namespace LaunchDarkly.Sdk
             foreach (var e in expectedItems)
             {
                 Assert.Contains(e, items);
-            }
-        }
-
-        public static WireMockServer NewServer()
-        {
-            return WireMockServer.Start(new WireMockServerSettings
-            {
-                Logger = new WireMockNullLogger(),
-                AllowAnyHttpStatusCodeInResponse = true // without this setting, WireMock will silently change errors like 429 to 200
-            });
-        }
-
-        public static void WithServer(Action<WireMockServer> a)
-        {
-            var server = NewServer();
-            try
-            {
-                a(server);
-            }
-            finally
-            {
-                server.Stop();
-            }
-        }
-
-        public static async Task WithServer(Func<WireMockServer, Task> a)
-        {
-            var server = NewServer();
-            try
-            {
-                await a(server);
-            }
-            finally
-            {
-                server.Stop();
             }
         }
     }
