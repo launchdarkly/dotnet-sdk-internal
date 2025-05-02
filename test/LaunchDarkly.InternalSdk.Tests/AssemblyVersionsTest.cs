bd0aa2b211bbce9487a797fcb0dc180a1964285c
jsonbailey <jbailey@launchdarkly.com>
2025-05-02 19:36:42 +0000

    net462 doesn't support string split

--- test/LaunchDarkly.InternalSdk.Tests/AssemblyVersionsTest.cs
+++ test/LaunchDarkly.InternalSdk.Tests/AssemblyVersionsTest.cs
@@ -9,7 +9,7 @@ namespace LaunchDarkly.Sdk.Internal
         public void GetVersionString()
         {
             // Starting in .NET 8, the commit sha is appended to the version string.
-            var versionString = AssemblyVersions.GetAssemblyVersionStringForType(typeof(AssemblyVersionsTest)).Split("+")[0];
+            var versionString = AssemblyVersions.GetAssemblyVersionStringForType(typeof(AssemblyVersionsTest)).Split('+')[0];
 
             Assert.Equal(
                 "1.2.3", // this is hard-coded in LaunchDarkly.InternalSdk.Tests.csproj
