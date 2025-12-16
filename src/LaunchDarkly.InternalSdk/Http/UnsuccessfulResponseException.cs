3cca2ff29e779def2fe75770dfdd13b94433742f
Todd Anderson <127344469+tanderson-ld@users.noreply.github.com>
2025-12-16 17:46:11 -0500

    feat: adds headers to UnsuccessfulResponseException (#52)

--- src/LaunchDarkly.InternalSdk/Http/UnsuccessfulResponseException.cs
+++ src/LaunchDarkly.InternalSdk/Http/UnsuccessfulResponseException.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LaunchDarkly.Sdk.Internal.Http
 {
@@ -10,10 +11,26 @@ namespace LaunchDarkly.Sdk.Internal.Http
             private set;
         }
 
+        /// <summary>
+        /// The HTTP headers from the response.
+        /// </summary>
+        public IEnumerable<KeyValuePair<string, IEnumerable<string>>> Headers { get; }
+
         public UnsuccessfulResponseException(int statusCode) :
+            this(statusCode, new Dictionary<string, IEnumerable<string>>())
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance with headers.
+        /// </summary>
+        /// <param name="statusCode">the HTTP status code of the response</param>
+        /// <param name="headers">the HTTP headers from the response</param>
+        public UnsuccessfulResponseException(int statusCode, IEnumerable<KeyValuePair<string, IEnumerable<string>>> headers) :
             base(string.Format("HTTP status {0}", statusCode))
         {
             StatusCode = statusCode;
+            Headers = headers ?? new Dictionary<string, IEnumerable<string>>();
         }
     }
 }
